2ff81d4df9722ecea24bb7ff1a36145ab305fdc8
Hamza RABIH <hamzarabih98@gmail.com>
2025-09-16 17:44:01 +0100

    feat(backend): add Customer search API

---
+++ ebanking-front-angular/src/app/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8081/'
+};
+/*
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8081/'
+};
+
+export const environment = {
+  production: true,
+  apiUrl: 'http://ws-01-prd:8081/'
+};
+
+ */
